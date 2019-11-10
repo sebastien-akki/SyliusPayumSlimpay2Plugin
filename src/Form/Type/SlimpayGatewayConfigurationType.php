ba93bf76ff06fe41dfa41e4347eb5594a1e8e8a5
sebastien.leprovost <sebastien@akki.fr>
2019-11-10 23:45:42 +0100

    version initiale

--- src/Form/Type/SlimpayGatewayConfigurationType.php
+++ src/Form/Type/SlimpayGatewayConfigurationType.php
@@ -8,6 +8,7 @@ use Akki\SyliusPayumSlimpayPlugin\Legacy\Slimpay;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
